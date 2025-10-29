3eb056f62bcc97c1b418194c77c24c789add8d28
permaboost370 <permaboost370@gmail.com>
2025-10-29 20:09:39 +0200

    Create start.py

---
+++ start.py
@@ -0,0 +1,7 @@
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", "8000"))
+    print(f"🚀 Starting app on port {port}")
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
